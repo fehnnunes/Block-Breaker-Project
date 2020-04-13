522cc64a82a7710264257ff92071286d2580c976
fehnnunes <felipe.nunes97@gmail.com>
2020-04-13 00:39:49 -0300

    Final Version 0.0
    Scenes, audios and images were added to finalize the game.

--- Block Breaker/Assets/Scripts/GameSession.cs
+++ Block Breaker/Assets/Scripts/GameSession.cs
@@ -10,8 +10,10 @@ public class GameSession : MonoBehaviour
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
 
     //state variables
+
     [SerializeField] int currentScore = 0;
 
     //Awake
@@ -51,5 +53,10 @@ public class GameSession : MonoBehaviour
     public void ResetGame()
     {
         Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
     }
 }
